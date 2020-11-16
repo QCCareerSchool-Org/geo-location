f9f8774f12668d7a3cc4d0e2863991c0d755240f
Dave Welsh <dave@qccareerschool.com>
2020-11-16 14:42:01 -0500

    Update helmet settings

--- src/index.ts
+++ src/index.ts
@@ -16,24 +16,25 @@ const corsOptions: cors.CorsOptions = {
   allowedHeaders: [ 'Content-Type' ],
   exposedHeaders: [ 'Cache-Control', 'X-Total' ],
   origin: [
-    /^(.*\.)?qccareerschool\.com$/,
-    /^(.*\.)?qcmakeupacademy\.com$/,
-    /^(.*\.)?qceventplanning\.com$/,
-    /^(.*\.)?qcdesignschool\.com$/,
-    /^(.*\.)?doggroomingcourse\.com$/,
-    /^(.*\.)?winghill\.com$/,
-    /^(.*\.)?qcwellnessstudies\.com$/,
-    /^https:\/\/qccareerschool-com-.*\.now\.sh$/,
-    /http:\/\/(.*\.)localhost:3000/,
-    'http://localhost:3000',
-    'http://localhost:4200',
-    'http://localhost:8000',
-    'http://192.168.6.197:3000',
-    'https://blissful-hopper-b5c7db.netlify.com',
-    /www-qcwellnessstudies-com\.now\.sh$/,
-    /\.qccareerschool\.now\.sh$/,
+    /(?:.*\.)?qccareerschool\.com$/,
+    /(?:.*\.)?qcmakeupacademy\.com$/,
+    /(?:.*\.)?qceventplanning\.com$/,
+    /(?:.*\.)?qcdesignschool\.com$/,
+    /(?:.*\.)?doggroomingcourse\.com$/,
+    /(?:.*\.)?winghill\.com$/,
+    /(?:.*\.)?qcwellnessstudies\.com$/,
+    /(?:.*\.)?localhost(?::\d{1,5})?$/,
   ],
 };
+
+// these are already done by the proxy
+const helmetOptions = {
+  hsts: false,
+  contentSecurityPolicy: false,
+  referrerPolicy: false,
+  noSniff: false,
+  frameguard: false,
+} as const;
 
 const HTTP_PORT = 15002;
 
@@ -45,7 +46,7 @@ router.get('/css', requestIp.mw(), cssHandler);
 
 const app: express.Express = express();
 app.use(compression());
-app.use(helmet());
+app.use(helmet(helmetOptions));
 app.use(cors(corsOptions));
 app.use('/geoLocation', router);
 app.use(httpErrorHandler);
