b3a63270f227593e8fd225fa7b1cc065aaaeb7fb
Dave Welsh <dave@qccareerschool.com>
2022-04-27 14:10:10 -0400

    Provide default province for US, CA, and AU when not detected

--- src/lib/getLocation.ts
+++ src/lib/getLocation.ts
@@ -8,18 +8,17 @@ export type GeoLocation = {
   provinceName: string | null;
 };
 
-export const getLocation = async (ip?: string): Promise<GeoLocation> => {
-  // prepare the result with default values
-  const result: GeoLocation = {
-    countryCode: 'US',
-    countryName: 'United States',
-    provinceCode: null,
-    provinceName: null,
-  };
+const defaultLocation: Readonly<GeoLocation> = {
+  countryCode: 'US',
+  countryName: 'United States',
+  provinceCode: 'MD',
+  provinceName: 'Maryland',
+};
 
+export const getLocation = async (ip?: string): Promise<GeoLocation> => {
   // if we can't determine the ip address, send the default values
   if (typeof ip === 'undefined') {
-    return result;
+    return defaultLocation;
   }
 
   // filter for the Office's IP address
@@ -27,23 +26,38 @@ export const getLocation = async (ip?: string): Promise<GeoLocation> => {
     return {
       countryCode: 'CA',
       countryName: 'Canada',
-      provinceCode: null,
-      provinceName: null,
+      provinceCode: 'ON',
+      provinceName: 'Ontario',
     };
   }
 
   const reader = await getReader();
 
   const response = reader.get(ip);
-  if (response !== null) { // gor a response
-    if (typeof response.country !== 'undefined') {
-      result.countryCode = response.country.iso_code;
-      result.countryName = response.country.names.en;
-    }
+  if (response !== null && typeof response.country !== 'undefined') { // got a response
+    const result: GeoLocation = {
+      countryCode: response.country.iso_code,
+      countryName: response.country.names.en,
+      provinceCode: null,
+      provinceName: null,
+    };
     if (typeof response.subdivisions !== 'undefined') {
       result.provinceCode = response.subdivisions[0].iso_code;
       result.provinceName = response.subdivisions[0].names.en;
+    } else {
+      if (result.countryCode === 'CA') {
+        result.provinceCode = 'ON';
+        result.provinceName = 'Ontario';
+      } else if (result.countryCode === 'US') {
+        result.provinceCode = 'MD';
+        result.provinceName = 'Maryland';
+      } else if (result.countryCode === 'AU') {
+        result.provinceCode = 'NSW';
+        result.provinceName = 'New South Wales';
+      }
     }
+    return result;
   }
-  return result;
+
+  return defaultLocation;
 };
