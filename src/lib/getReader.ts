d2d6e203a2fe370d072719ad369f0fe21f38b34c
Dave Welsh <dave@qccareerschool.com>
2026-03-23 15:08:15 -0400

    Added geoipfiles

--- src/lib/getReader.ts
+++ src/lib/getReader.ts
@@ -1,9 +1,10 @@
 import type { CityResponse, Reader } from 'maxmind';
 import maxmind from 'maxmind';
+import path from 'node:path';
 
 let reader: Reader<CityResponse> | undefined = undefined;
 
 export const getReader = async (): Promise<Reader<CityResponse>> => {
-  reader ??= await maxmind.open('/usr/share/GeoIP/GeoLite2-City.mmdb');
+  reader ??= await maxmind.open(path.join(process.cwd(), 'maxmind/GeoLite2-City.mmdb'));
   return reader;
 };
