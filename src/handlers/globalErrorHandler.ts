1fc226fce04677657603caefb0b28803a8390af3
Dave Welsh <dave@qccareerschool.com>
2026-03-24 14:21:52 -0400

    Remove logger

--- src/handlers/globalErrorHandler.ts
+++ src/handlers/globalErrorHandler.ts
@@ -1,9 +1,7 @@
 import type { ErrorRequestHandler } from 'express';
 
-import { logger } from '../logger';
-
 export const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  logger.error(err);
+  console.error(err);
   if (!res.headersSent) {
     res.status(500).send(JSON.stringify(err));
   } else {
